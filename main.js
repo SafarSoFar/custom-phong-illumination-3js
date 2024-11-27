f8886669457ed81d3d59fafb953b2e6bfa39e4cb
Safar Isaev <133248217+SafarSoFar@users.noreply.github.com>
2024-11-27 02:08:15 -0700

    Normal mapping, normal map toggling

--- main.js
+++ main.js
@@ -10,11 +10,14 @@ const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer(); 
 
 
+// scene.background = new THREE.Color(0x555555);
 renderer.setSize( window.innerWidth, window.innerHeight ); 
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); 
 
 const controls = new OrbitControls(camera, renderer.domElement );
+
+var phongShaderMat;
 
 let settings = {
      lightSourceXPos: -1.3,
@@ -27,29 +30,42 @@ let settings = {
      ambientLightIntensity: 0.5,
      shininessFactor: 32.0,
      shininessIntensity: 0.0,
+
      displacementStrength: 0.04,
+     toggleNormalMap: true,
 }
 
-let gui = new GUI({title: "Lighting settings"});
-gui.add(settings, "lightIntensity", 0.0, 1.0);
-gui.add(settings, "ambientLightIntensity", 0.0, 1.0);
-gui.add(settings, "lightSourceXPos", -10.0, 10.0).onChange((val) => {
+let lightingGui = new GUI({title: "Lighting settings"});
+lightingGui.add(settings, "lightIntensity", 0.0, 1.0);
+lightingGui.add(settings, "ambientLightIntensity", 0.0, 1.0);
+lightingGui.add(settings, "lightSourceXPos", -10.0, 10.0).onChange((val) => {
      lightSourcePos.x = val;
 })
-gui.add(settings, "lightSourceYPos", -10.0, 10.0).onChange((val) => {
+lightingGui.add(settings, "lightSourceYPos", -10.0, 10.0).onChange((val) => {
      lightSourcePos.y = val;
 });
-gui.add(settings, "lightSourceZPos", -10.0, 10.0).onChange((val) => {
+lightingGui.add(settings, "lightSourceZPos", -10.0, 10.0).onChange((val) => {
      lightSourcePos.z = val;
 });
 
-gui.add(settings, "shininessFactor", 1.0, 256);
-gui.add(settings, "shininessIntensity", 0.0, 1.0);
-gui.add(settings, "displacementStrength", 0.0, 1.0, 0.01);
+lightingGui.add(settings, "shininessFactor", 1.0, 256);
+lightingGui.add(settings, "shininessIntensity", 0.0, 1.0);
+lightingGui.addColor(settings, "mainColor");
+lightingGui.addColor(settings, "lightSourceColor");
+lightingGui.addColor(settings, "ambientLightColor");
 
-gui.addColor(settings, "mainColor");
-gui.addColor(settings, "lightSourceColor");
-gui.addColor(settings, "ambientLightColor");
+let textureGui = new GUI({title: "Texture Control"});
+textureGui.add(settings, "displacementStrength", 0.0, 1.0, 0.01);
+textureGui.add(settings, "displacementStrength", 0.0, 1.0, 0.01);
+textureGui.add(settings, "toggleNormalMap").onChange( (val) =>{
+     phongShaderMat.uniforms.uToggleNormalMap.value = val;
+});
+
+const guiElement = textureGui.domElement;
+guiElement.style.position = 'absolute';
+guiElement.style.top = '0px';         
+guiElement.style.left = '0px';        
+
 document.body.appendChild( renderer.domElement );
 
 let loader = new THREE.TextureLoader();
@@ -61,13 +77,15 @@ var displacementMap = loader.load("assets/displacement-brick.jpg");
 displacementMap.wrapS = THREE.RepeatWrapping;
 displacementMap.wrapT = THREE.RepeatWrapping;
 
-var normalMap = loader.load("assets/normal");
+var normalMap = loader.load("assets/normal-brick.jpg");
+normalMap.wrapS = THREE.RepeatWrapping;
+normalMap.wrapT = THREE.RepeatWrapping;
 
 let lightSourcePos = new THREE.Vector3(3.0,5.0,3.0);
 let directionalLightHelper = new THREE.Mesh(new THREE.SphereGeometry(), new THREE.MeshBasicMaterial({color: 0xffffff}));
 scene.add(directionalLightHelper);
 
-let lightDependentShaderMat = new THREE.ShaderMaterial({
+phongShaderMat = new THREE.ShaderMaterial({
      vertexShader: vertexShader(),
      fragmentShader: fragmentShader(),
      uniforms: {
@@ -84,11 +102,13 @@ let lightDependentShaderMat = new THREE.ShaderMaterial({
 
           uDisplacementMap: {value: displacementMap},
           uDisplacementStrength: {value: settings.displacementStrength},
+          uNormalMap: {value: normalMap},
+          uToggleNormalMap: {value: true},
      },
      transparent: true,
 })
 
-let sphere = new THREE.Mesh(new THREE.PlaneGeometry(1,1,1000,1000), lightDependentShaderMat);
+let sphere = new THREE.Mesh(new THREE.PlaneGeometry(1,1,2000,2000), phongShaderMat);
 scene.add(sphere);
 
 
@@ -98,16 +118,16 @@ camera.position.z = 5;
 function animate() {
 
      directionalLightHelper.position.copy(lightSourcePos);
-     lightDependentShaderMat.uniforms.uLightSourcePos.value = lightSourcePos;
-     lightDependentShaderMat.uniforms.uLightIntensity.value = settings.lightIntensity;
-     lightDependentShaderMat.uniforms.uCameraPos.value = camera.position;
-     lightDependentShaderMat.uniforms.uMainColor.value = settings.mainColor;
-     lightDependentShaderMat.uniforms.uAmbientLightColor.value = settings.ambientLightColor;
-     lightDependentShaderMat.uniforms.uAmbientLightIntensity.value = settings.ambientLightIntensity;
-     lightDependentShaderMat.uniforms.uLightSourceColor.value = settings.lightSourceColor;
-     lightDependentShaderMat.uniforms.uShininessFactor.value = settings.shininessFactor;
-     lightDependentShaderMat.uniforms.uShininessIntensity.value = settings.shininessIntensity;
-     lightDependentShaderMat.uniforms.uDisplacementStrength.value = settings.displacementStrength;
+     phongShaderMat.uniforms.uLightSourcePos.value = lightSourcePos;
+     phongShaderMat.uniforms.uLightIntensity.value = settings.lightIntensity;
+     phongShaderMat.uniforms.uCameraPos.value = camera.position;
+     phongShaderMat.uniforms.uMainColor.value = settings.mainColor;
+     phongShaderMat.uniforms.uAmbientLightColor.value = settings.ambientLightColor;
+     phongShaderMat.uniforms.uAmbientLightIntensity.value = settings.ambientLightIntensity;
+     phongShaderMat.uniforms.uLightSourceColor.value = settings.lightSourceColor;
+     phongShaderMat.uniforms.uShininessFactor.value = settings.shininessFactor;
+     phongShaderMat.uniforms.uShininessIntensity.value = settings.shininessIntensity;
+     phongShaderMat.uniforms.uDisplacementStrength.value = settings.displacementStrength;
 
 
      renderer.render( scene, camera ); 
@@ -131,12 +151,23 @@ function vertexShader() {
 
           uniform sampler2D uDisplacementMap;
           uniform float uDisplacementStrength;
+
+          uniform sampler2D uNormalMap;
+          uniform bool uToggleNormalMap;
           
           void main() {
 
                vUv = position; 
                vec3 pos = position;
-               vNormal = normalize(normal);
+
+               if(uToggleNormalMap){
+                    vec3 normalMap = texture2D(uNormalMap, vUv.xy).rgb;
+                    vNormal = normalize(normalMap * 2.0 -1.0);
+               }
+               else{
+                    vNormal = normalize(normal);
+               }
+               
 
                float displacement = texture2D(uDisplacementMap, vUv.xy).r;
                displacement *= uDisplacementStrength;
@@ -194,7 +225,7 @@ function onDocumentKeyDown(event) {
      
     var keyCode = event.which;
     if (keyCode == 72) { // 'h' ascii value
-        gui.show(gui._hidden);
+        lightingGui.show(lightingGui._hidden);
     }
 };
 
